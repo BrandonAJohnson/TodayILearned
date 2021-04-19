f3eaecef0df123c4d2f05af351eb13d40bfab3f9
Brandon Johnson <brandonajohnson09@gmail.com>
2021-04-18 22:21:37 -0400

    added post details

--- feed/models.py
+++ feed/models.py
@@ -1,8 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Post(models.Model):
 	text = models.CharField(max_length=240)
+	title = models.CharField(max_length=80, null=True)
+	details = models.TextField(null=True)
 	date = models.DateTimeField(auto_now=True)
+	tag = models.CharField(max_length=32, null=True)
+	author = models.ForeignKey(
+		User,
+		on_delete=models.CASCADE, # when user is deleted, delete posts
+	)
 
 	def __str__(self):
 		return self.text[0:100]
--- feed/views.py
+++ feed/views.py
@@ -1,9 +1,15 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 class HomePage(ListView):
 	http_method_names = ["get"]
-	template_name ="feed/homepage.html"
+	template_name = "feed/homepage.html"
 	model = Post
 	context_object_name = "posts"
 	queryset = Post.objects.all().order_by("-id")[0:30]
+
+class PostDetailView(DetailView):
+	http_method_names = ["get"]
+	template_name = "feed/detail.html"
+	model = Post
+	context_object_name = "post"
